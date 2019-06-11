2e6d4bd5fe46dfad0064a5fb06735927984b3ce2
Helder Lee <helderlee@yahoo.com>
2019-06-11 16:17:12 -0300

    Middleware

---
+++ L013-Middleware/resources/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  databaseStatus: {
+    DATABASE_CONNECTED: 'Database connected successfully',
+    DATABASE_ERROR: 'Database error',
+    ENTITY_CREATED: 'Entity Created',
+    ENTITY_MODIFIED: 'Entity Modified',
+    ENTITY_FETCHED: 'Entity Fetched',
+    ENTITY_DELETED: 'Entity Deleted'
+  },
+  controllerStatus: {
+    BAD_REQUEST: 'Required fields missing'
+  },
+  serviceStatus: {
+    USER_CREATED: 'User created successfully'
+  }
+}
