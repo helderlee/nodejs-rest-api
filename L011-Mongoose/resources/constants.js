41686c9b4610809e32d1b1eb89e042a45900868c
Helder Lee <helderlee@yahoo.com>
2019-06-11 15:26:01 -0300

    Mongoose

---
+++ L011-Mongoose/resources/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  databaseStatus: {
+    DATABASE_CONNECTED: 'Database connected successfully',
+    DATABASE_ERROR: 'Database error',
+    ENTITY_CREATED: 'Entity Created',
+    ENTITY_MODIFIED: 'Entity Modified',
+    ENTITY_FETCHED: 'Entity Fetched',
+    ENTITY_DELETED: 'Entity Deleted'
+  }
+}
